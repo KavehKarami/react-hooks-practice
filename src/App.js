300187fd03c881da7e5f801bf6e238b394f56001
Kaveh Karami <Kaveh.Karami.KK@gmail.com>
2020-06-21 03:02:38 -0700

    react hooks useEffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  // our starte
+  const [count, setCount] = useState(null);
+
+  //bad way
+  const fruitStateVariable = useState("banana");
+
+  // our effect
+  useEffect(() => {
+    console.log(count);
+  }, [count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div
+        onClick={() => {
+          setCount(count + 1);
+          fruitStateVariable[1]("hi");
+        }}
+        className="btn btn-success mb-3"
+      >
+        click
+      </div>
+
+      <div>{fruitStateVariable[0]}</div>
+
+      <div>{count}</div>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
